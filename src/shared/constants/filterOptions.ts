8cc76f53e96f75747659a5dd86941ea0d99734f7
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-03-11 17:19:42 +0200

    Filtering options added

---
+++ src/shared/constants/filterOptions.ts
@@ -0,0 +1,7 @@
+const FILTER_OPTIONS = [
+  { value: 'all', label: 'All' },
+  { value: 'oceania', label: 'Oceania region' },
+  { value: 'smaller', label: 'Smaller than Lithuania' },
+];
+
+export default FILTER_OPTIONS;
